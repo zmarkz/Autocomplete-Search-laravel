6e784746a183c96fc8044896a6e1479d0602770d
Markandey <markandey.singh@opinioapp.com>
2016-08-08 19:21:17 +0530

    added textboxes to the homepage

---
+++ humbell/app/views/home.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HomePage</title>
+    <script src="http://cdn.jsdelivr.net/typeahead.js/0.9.3/typeahead.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div style="margin-top: 20%;margin-left: 30%">
+    <form class="form-horizontal">
+        <div class="form-group">
+            <div class="col-xs-10">
+                <div class="form-inline">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Category"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Item"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
